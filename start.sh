2d1cfacfc5e93fc634c1dfa41effe2f4f00a4892
Mohammed Saeed <mohammedsaead643@gmail.com>
2025-10-19 22:59:14 +0300

    Fix Railway deployment - Use .NET 8.0 and simplify configuration

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 cd NordenAPI
-
-# Install .NET 9.0 SDK
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 9.0.0
-export PATH="$HOME/.dotnet:$PATH"
-
 dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -o ./publish
